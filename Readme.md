67753827ada848808a8be3d6842be781f47a7dfa
osick <oliver.sick@web.de>
2024-12-27 20:02:51 +0100

    added Readme, sample csv and help in app.py

---
+++ Readme.md
@@ -0,0 +1,29 @@
+# Stock Portfolio app
+
+## Intro
+
+Simple proof of concept of a Portfolio app to monitor and view the history of a portfilio.
+
+## Usage
+
+    $> streamlit run app.py
+
+## Input für Portfolio
+
+### Structure of the CSV file  
+    | NAME      | ISIN         | AMOUNT | BUY  | DATE       | SYMBOL | 
+    |-----------|--------------|--------|------|------------|--------|  
+    | ALLIANZ   | DE0008404005 | 11     | 2000 | 31.10.2021 | ALIZF  |
+    | MICROSOFT | US5949181045 | 15     | 1000 | 31.10.2022 | MSFT   |
+    | ...       | ...          | ...    | ...  | ...        | ...    |
+    | ...       | ...          | ...    | ...  | ...        | ...    |
+    
+    Each line is a buy or sell transaction
+
+### Columns
+**NAME**: arbitrary Identifier  
+**ISIN**: ISIN Number (optional)  
+**AMOUNT**: Number of shares  
+**BUY**: The total price (in EUR)  
+**DATE**: Buying Day (no Time or timezone Info nescessary)  
+**SYMBOL**: The symbol of the share 
--- app.py
+++ app.py
@@ -6,43 +6,109 @@ import plotly.graph_objects as go
 from prophet import Prophet
 from datetime import datetime
 
+
+@st.cache_data
+def get_portfolio(csvfile):
+    portfolio = pd.read_csv(csvfile, sep=",")
+    portfolio["DATE"]= pd.to_datetime(portfolio['DATE'])
+    portfolio.sort_values("DATE", inplace=True, ascending=True)
+    portfolio.set_index("DATE", inplace=True)
+    portfolio.index = portfolio.index.tz_localize(None)
+    return portfolio
+
+@st.cache_data
+def get_exchange_rates(start,end):
+    rates = ['USDEUR=X', 'GBPEUR=X']
+    tickers = yf.Tickers(' '.join(rates))
+    start = '2022-10-31'
+    exchange_rates = []
+    for i in tickers.tickers: exchange_rates.append(tickers.tickers[i].history(start=start, end=today).Close)
+    ex_df = pd.DataFrame(exchange_rates).T
+    ex_df.columns = rates
+    ex_df['EUREUR=X'] = 1.0
+    ex_df.index = ex_df.index.tz_localize(None)
+    return ex_df
+
 if __name__ == "__main__":
-    
     st.set_page_config(layout="wide")
-    portfolio = pd.read_csv("account.csv", sep=";")
-    fig = go.Figure()
-    df_combined = pd.DataFrame()
-    lst_symbols=[]
 
-    for index, row in portfolio.iterrows():
-        symbol = row["SYMBOL"]
-        lst_symbols.append(symbol)
-        start_date = datetime.strptime( row["Date"], '%d.%m.%Y')
-        amount = row["AMOUNT"]
+    # Streamlit App
 
-        try:
-            ticker = yf.Ticker(symbol)
-            ticker_df = ticker.history(start=start_date, end = datetime.now())
-            df_combined[symbol] = ticker_df["Close"] * amount
-            #fig.add_trace(go.Scatter(x=ticker_df.index, y=ticker_df['Close'], mode='lines', name=symbol))
+    # Upload CSV File
+    help ="""
+    ##### Structure of the CSV file  
+    | NAME      | ISIN         | AMOUNT | BUY  | DATE       | SYMBOL | 
+    |-----------|--------------|--------|------|------------|--------|  
+    | ALLIANZ   | DE0008404005 | 11     | 2000 | 31.10.2021 | ALIZF  |
+    | MICROSOFT | US5949181045 | 15     | 1000 | 31.10.2022 | MSFT   |
+    | ...       | ...          | ...    | ...  | ...        | ...    |
+    | ...       | ...          | ...    | ...  | ...        | ...    |
+    
+    Each line is a buy or sell transaction
 
-        except Exception as e:
-            st.warning(f"Failed to retrieve data for {symbol}: {e}")
+    #### Columns
+    **NAME**: arbitrary Identifier  
+    **ISIN**: ISIN Number (optional)  
+    **AMOUNT**: Number of shares  
+    **BUY**: The total price (in EUR)  
+    **DATE**: Buying Day (no Time or timezone Info nescessary)  
+    **SYMBOL**: The symbol of the share  
+    """
+    c1, c2 = st.columns([3,8], border=False, gap="small")
 
-    st.write(df_combined)
-    df_combined['Close'] = df_combined[lst_symbols].sum(axis=1)
-    fig.add_trace(go.Scatter(x=df_combined.index, y=df_combined['Close'], mode='lines', name="Total"))
+    with c1:
+        st.title("Portfolio Viewer")
+        with st.popover("Help"): st.markdown(help)
+        uploaded_file = st.file_uploader("Upload a CSV file", type="csv")
 
-    # Update graph layout
-    fig.update_layout(
-        title="Stock Closing Prices",
-        xaxis_title="Date",
-        yaxis_title="Close Price (USD)",
-        template="plotly_dark",
-        
-        height=800
-        
-    )
+    if uploaded_file:
+        with st.spinner("Computing the Portfolio Chart..."):
+            portfolio = get_portfolio(uploaded_file)
+            with c2:
+                st.title("Portfolio History")
+                st.dataframe(portfolio, use_container_width= True)
+            symbols={s:[] for s in list(set(list(portfolio["SYMBOL"])))}
 
-        # Display graph
-    st.plotly_chart(fig,use_container_width=True,height=800)
+            today = datetime.now()
+            ex_df = get_exchange_rates("2022-10-31",today)
+
+            fig = go.Figure()    
+
+            df_combined = pd.DataFrame()
+            for index, row in portfolio.iterrows():
+                try:
+                    symbols[row["SYMBOL"]].append(index)
+                    ticker = yf.Ticker(row["SYMBOL"])
+                    exchange_factor = ex_df[f'{ticker.info["currency"]}EUR=X']
+
+                    ticker_df = ticker.history(start=index, end=today)
+                    ticker_df.index = ticker_df.index.tz_localize(None)
+                    # TOTAL Value per share
+                    df_combined[f'{row["SYMBOL"]}_{index}_close'] = ticker_df["Close"] * row["AMOUNT"] * exchange_factor
+                    df_combined[f'{row["SYMBOL"]}_{index}_close'].interpolate(inplace=True) 
+
+                    # BUY Value per share
+                    ticker_df["BUY"]= row["BUY"]
+                    df_combined[f'{row["SYMBOL"]}_{index}_buy'] = ticker_df["BUY"]
+                    df_combined[f'{row["SYMBOL"]}_{index}_buy'].interpolate(inplace=True) 
+
+                except Exception as e:
+                    st.warning(f"Failed to retrieve data for {symbols[-1]}: {e}")
+
+
+            df_combined[f'_close'] = df_combined[[f"{symbol}_{index}_close" for symbol in symbols.keys() for index in symbols[symbol] ]].sum(axis=1)
+            df_combined[f'_buy']   = df_combined[[f"{symbol}_{index}_buy"   for symbol in symbols.keys() for index in symbols[symbol] ]].sum(axis=1)   
+            df_combined[f'_win']   = df_combined[f'_close'] - df_combined[f'_buy']
+            
+
+            fig.add_trace(go.Scatter(x=df_combined.index, y=df_combined[f'_close'], mode='lines', name=f"Total"))
+            fig.add_trace(go.Scatter(x=df_combined.index, y=df_combined[f'_buy'], mode='lines', name=f"Buy"))
+            fig.add_trace(go.Scatter(x=df_combined.index, y=df_combined[f'_win'], mode='lines', name=f"Win"))
+
+
+            st.header("History Chart")
+            # Update graph layout
+            fig.update_layout(title="", xaxis_title="Date", yaxis_title="EUR", template="plotly_dark", height=800)
+            fig.update_layout(yaxis_range=[0,df_combined["_close"].max()])
+            # Display graph
+            st.plotly_chart(fig,use_container_width=True,height=800)
